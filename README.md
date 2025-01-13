77741e4044631ffb5a4ffe01dc26f4734f4e9560
Alexandru-Manuel Carabus <manue97132@gmail.com>
2025-01-13 11:28:09 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### Installation instructions
+
+```bash
+composer require hawksama/magento2-admin-menu
+```
---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Hawksama_AdminMenu',
+    __DIR__
+);
